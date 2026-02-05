45651030e36c0a1c83bf63e64e60d2de515ac99e
Samnang-Vouen <vouensamnang@gmail.com>
2026-02-05 11:36:28 +0700

    BLA-001

--- blabla_car/lib/services/rides_service.dart
+++ blabla_car/lib/services/rides_service.dart
@@ -1,4 +1,5 @@
 import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
 ////
@@ -6,7 +7,28 @@ import '../model/ride/ride.dart';
 ///   - The list of available rides
 ///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
- 
+  static List<Ride> filterByDeparture(Location departure) {
+    return availableRides
+        .where((e) => e.departureLocation == departure)
+        .toList();
+  }
+
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return availableRides
+        .where((e) => e.availableSeats == seatRequested)
+        .toList();
+  }
+
+  static bool filterBy({Location? departure, int? seatRequested}) {
+    final matchingDeparture = availableRides.where(
+      (e) => e.departureLocation == departure,
+    ).toList();
+
+    final matchingSeatRequested = availableRides.where(
+      (e) => e.availableSeats >= seatRequested!,
+    ).toList();
+    return matchingDeparture && matchingSeatRequested;
+  }
 }
