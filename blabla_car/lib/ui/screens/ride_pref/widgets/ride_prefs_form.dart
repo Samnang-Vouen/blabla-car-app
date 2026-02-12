9d1ceb1c4bac99b696bb24bc5dafc8fd51e97b0a
Samnang-Vouen <vouensamnang@gmail.com>
2026-02-12 10:48:00 +0700

    BLA-003 Implement Form (not complete yet)

--- blabla_car/lib/ui/screens/ride_pref/widgets/ride_prefs_form.dart
+++ blabla_car/lib/ui/screens/ride_pref/widgets/ride_prefs_form.dart
@@ -1,3 +1,5 @@
+import 'package:blabla_car/ui/widgets/display/bla_divider.dart';
+import 'package:blabla_car/utils/date_time_utils.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../model/ride/locations.dart';
@@ -55,7 +57,63 @@ class _RidePrefFormState extends State<RidePrefForm> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [ 
-        
+          RidePrefButton(
+                icon: Icons.circle_outlined,
+                text: departure?.name ?? 'Leaving from',
+                onPressed: () {
+                  
+                },
+              ),
+              const BlaDivider(),
+              RidePrefButton(
+                icon: Icons.circle_outlined,
+                text: arrival?.name ?? 'Going to',
+                onPressed: () {
+                  
+                },
+              ),
+              const BlaDivider(),
+              RidePrefButton(
+                icon: Icons.calendar_month,
+                text: DateTimeUtils.formatDateTime(departureDate),
+                onPressed: () {
+                  
+                },
+              ),
+              const BlaDivider(),
+              RidePrefButton(
+                icon: Icons.people_alt,
+                text: '$requestedSeats',
+                onPressed: () {
+                  
+                },
+              ),
         ]);
+  }
+}
+
+class RidePrefButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final VoidCallback? onPressed;
+
+  const RidePrefButton({
+    super.key,
+    required this.icon,
+    required this.text,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed ?? () {},
+      style: TextButton.styleFrom(padding: EdgeInsets.zero),
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(text),
+        contentPadding: EdgeInsets.zero,
+      ),
+    );
   }
 }
