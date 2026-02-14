66faf74ecb5172a8cc59d0da473da1e4e567a8f5
Samnang-Vouen <vouensamnang@gmail.com>
2026-02-14 16:20:24 +0700

    BLA-002 Implement BlaButton (Correction)

--- blabla_car/lib/ui/widgets/actions/bla_button.dart
+++ blabla_car/lib/ui/widgets/actions/bla_button.dart
@@ -2,7 +2,7 @@ import 'package:blabla_car/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 ///
-/// The BlaButton widget is a reusable button component used 
+/// The BlaButton widget is a reusable button component used
 /// throughout the application.
 ///
 /// It allows:
@@ -12,47 +12,75 @@ import 'package:flutter/material.dart';
 /// - Handling user tap actions via the onPressed callback
 ///
 
-class BlaButton extends StatelessWidget {
-  const BlaButton(
-      {super.key,
-      required this.text,
-      required this.textColor,
-      required this.buttonBackgroundColor,
-      required this.icon,
-      required this.iconColor,
-      required this.onPressed});
+enum ButtonType {
+  primary,
+  secondary,
+}
 
+class BlaButton extends StatelessWidget {
   final String text;
-  final Color textColor;
-  final Color buttonBackgroundColor;
-  final IconData? icon;
-  final Color iconColor;
   final VoidCallback? onPressed;
+  final ButtonType type;
+  final IconData? icon;
+  final BorderRadius? borderRadius;
+
+  const BlaButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.type = ButtonType.primary,
+    this.icon,
+    this.borderRadius,
+  });
 
   @override
   Widget build(BuildContext context) {
-     return ElevatedButton(
-      onPressed: onPressed ?? () {}, 
-      style: ElevatedButton.styleFrom(
-        backgroundColor: buttonBackgroundColor,
-        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          if (icon != null) ...[
-            Icon(icon, color: iconColor, size: 20),
-            SizedBox(width: 8),
-          ],
-          Text(
-            text, 
-            style: BlaTextStyles.button.copyWith(color: textColor)
+    // Compute the rendering
+    Color backgroundColor =
+        type == ButtonType.primary ? BlaColors.primary : BlaColors.white;
+
+    BorderSide border = type == ButtonType.primary
+        ? BorderSide.none
+        : BorderSide(color: BlaColors.greyLight, width: 2);
+
+    Color textColor =
+        type == ButtonType.primary ? BlaColors.white : BlaColors.primary;
+
+    Color iconColor =
+        type == ButtonType.primary ? BlaColors.white : BlaColors.primary;
+
+    // Create the button icon - if any
+    List<Widget> children = [];
+    if (icon != null) {
+      children.add(Icon(icon, size: 20, color: iconColor));
+      children.add(SizedBox(width: BlaSpacings.s));
+    }
+
+    // Create the button text
+    Text buttonText = Text(
+      text,
+      style: BlaTextStyles.button.copyWith(color: textColor),
+    );
+
+    children.add(buttonText);
+
+    // Render the button
+    return SizedBox(
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          padding: EdgeInsets.symmetric(vertical: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius:
+                borderRadius ?? BorderRadius.circular(BlaSpacings.radius),
           ),
-        ],
+          side: border,
+        ),
+        onPressed: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: children,
+        ),
       ),
     );
   }
